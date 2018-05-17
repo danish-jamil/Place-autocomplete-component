1922c3fbbf48a57c4ed8404e0c1620101b80848f
danish-jamil <mrdanishjameel@gmail.com>
2018-05-17 01:35:41 -0700

    Inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  place: any;
+  logAddress($event) {
+    console.log($event);
+    this.place = $event;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { PlacesAutocompleteComponent } from './places-autocomplete/places-autocomplete.component';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, PlacesAutocompleteComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCvx3nJx_sh8miTnIHofk23FfoT86SgHak',
+      libraries: ['places']
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
